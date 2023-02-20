6dd67e9bb4c76f046e5413c0ef2985c6aac2db97
diego-ismail <die.ibarra@gmail.com>
2023-02-19 22:06:13 -0300

    trabajo final

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const cors = require("cors")
 require("dotenv").config()
 
 const app = express()
-//const indexRouter = require("./routes/index")
-//const apiRouter = require("./routes/api")
+const indexRouter = require("./routes/index")
+const userRouter = require("./routes/user")
 const {connect} = require("./db/db")
 
 app.use(logger("dev"))
 app.use(cors())
 app.use(express.json())
 
-//app.use( "/", indexRouter )
-//app.use( "/api", apiRouter )
+app.use( "/", indexRouter )
+app.use("/usuarios", userRouter);
 
 connect()
 
